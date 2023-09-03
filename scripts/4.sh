1e1a9bf109838426858ea9a2fd5be490d1ddbbe3
yanwei <yanruogu@gmail.com>
2023-09-03 03:11:45 +0000

    fix .

--- scripts/4.sh
+++ scripts/4.sh
@@ -3,6 +3,22 @@
 kubectl create ns istio-system
 kubectl create sa service-account-web -n istio-system
 
+cat > /tmp/role1.yaml << EOF
+apiVersion: rbac.authorization.k8s.io/v1
+kind: Role
+metadata:
+  name: role1
+  namespace: istio-system
+rules:
+- apiGroups:
+  - ""
+  resources:
+  - pods
+  verbs:
+  - create
+  - watch
+EOF
+
 cat > /tmp/test-web-pod.yaml << EOF
 apiVersion: v1
 kind: Pod
@@ -23,4 +39,6 @@ spec:
 status: {}
 EOF
 
+kubectl apply -f /tmp/role1.yaml
+kubectl create rolebinding sa-dev-1 --serviceaccount=istio-system:service-account-web --role=role1 -n istio-system
 kubectl apply -f /tmp/test-web-pod.yaml
